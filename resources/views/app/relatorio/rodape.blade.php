5fb55126aceca47cd096d4682d070c9ba56b6231
Rafael <rafael.marques-98@hotmail.com>
2021-11-25 20:09:10 -0300

    Atualizando

--- resources/views/app/relatorio/rodape.blade.php
+++ resources/views/app/relatorio/rodape.blade.php
@@ -1,7 +1,7 @@
 </div>
 <div class="">
     <footer class="" style="position: absolute; bottom: 0; text-align:center;">
-        <h4 class="text-center fs-6 fst-italic text-white-50"> Registro financeiro - Projeto realizado somente para fins academicos.</h4>
+        <h4 class="text-center fs-6 fst-italic text-white-50" style="margin-bottom: 4px;"> Registro financeiro - Projeto realizado somente para fins academicos.</h4>
         <p class="m-0 text-center fst-italic text-secondary" style='font-size: 12px'>Faculdade Pitagoras - Alunos: Cristiano Coelho, Marcelo freire e Rafael Maques</p>
         <p class="m-0 text-center fst-italic text-secondary" style='font-size: 12px'>&copy; Novembro - 2021</p>
     </footer>
